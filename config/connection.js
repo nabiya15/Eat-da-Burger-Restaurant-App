d94c4c6550e4f82742401e9941e0abfbe49d1f81
m.nabiya@gmail.com <m.nabiya@gmail.com>
2017-11-12 16:34:37 -0600

    JawsDB url edit

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-	port : 3306,
-	host : "localhost",
-	user : "root",
-	password : "Nabi786110",
-	database : "burger_db"
-})
-
+var connection; 
+if(process.env.JAWSDB_URL){
+	connection=mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+	connection = mysql.createConnection({
+		port : 3306,
+		host : "localhost",
+		user : "root",
+		password : "Nabi786110",
+		database : "burger_db"
+	})
+}
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
